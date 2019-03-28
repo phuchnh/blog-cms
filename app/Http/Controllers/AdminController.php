97f16171c5f77129240f42b8fc5d7ed95911eb82
Huỳnh Ngọc Hoàng Phúc <huynhngochoangphuc@gmail.com>
2019-03-28 18:32:30 +0700

    Add auth for admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,17 @@ namespace App\Http\Controllers;
 
 class AdminController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('auth.admin');
+    }
+
     public function __invoke()
     {
         return view('admin.app');
