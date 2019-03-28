ec5db88b97dd7c9ae8646ea4e03c4b6adeee50a1
Huỳnh Ngọc Hoàng Phúc <huynhngochoangphuc@gmail.com>
2019-03-28 17:44:59 +0700

    Init layout admin

--- resources/assets/js/components/Admin.vue
+++ resources/assets/js/components/Admin.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <h1>{{ messsage }}</h1>
+    <router-view></router-view>
   </div>
 </template>
 
--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -1,10 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Admin from './components/Admin';
 
 Vue.use(Router);
 
 const Foo = { template: '<div>Foo</div>' };
-const Foz = { template: '<div>Foz</div>' };
+const Dashboard = { template: '<div>Dashboard</div>' };
 const Bar = { template: '<div>Bar</div>' };
 const PageNotFound = { template: '<div>PageNotFound</div>' };
 
@@ -13,10 +14,12 @@ export function createRouter() {
         mode: 'history',
         routes: [
             {
-                path: 'admin',
+                path: '/admin',
                 name: 'admin',
+                component: Admin,
+                redirect: { name: 'dashboard' },
                 children: [
-                    { path: 'foz', name: 'foz', component: Foz },
+                    { path: 'dashboard', name: 'dashboard', component: Dashboard },
                     { path: 'foo', name: 'foo', component: Foo },
                     { path: 'bar', name: 'bar', component: Bar },
                     { path: '*', name: 'pageNotFound', component: PageNotFound }
