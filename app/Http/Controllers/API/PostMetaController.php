26b5a2a12ad4fcd5de8e91790c57759a845af07d
Lân Lê Duy <leduylan309@gmail.com>
2019-04-08 19:15:12 +0700

    fix bug

--- app/Http/Controllers/API/PostMetaController.php
+++ app/Http/Controllers/API/PostMetaController.php
@@ -81,9 +81,12 @@ class PostMetaController extends ApiBaseController
     {
         $inputArray = $request->validated();
 
-        $post->meta()
-             ->whereIn('meta_key', array_column($inputArray, 'meta_key'))
-             ->saveMany($this->createInstancePostMeta($inputArray));
+        $metaTemp = $post->meta()
+                         ->whereIn('meta_key', array_column($inputArray, 'meta_key'));
+
+        $metaTemp->delete();
+
+        $metaTemp->saveMany($this->createInstancePostMeta($inputArray));
 
         return $this->noContent();
     }
@@ -114,7 +117,9 @@ class PostMetaController extends ApiBaseController
     private function createInstancePostMeta($inputArray)
     {
         foreach ($inputArray as $input) {
-            $instance[] = new PostMeta($input);
+            if ($input['meta_value']) {
+                $instance[] = new PostMeta($input);
+            }
         }
 
         return $instance;
