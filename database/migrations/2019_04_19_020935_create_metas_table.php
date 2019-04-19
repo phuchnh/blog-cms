ad3ecd22eab12398bf17de93f824cee33dde3408
LeonNguyen <n.anhtuan0808@gmail.com>
2019-04-19 09:35:51 +0700

    init

---
+++ database/migrations/2019_04_19_020935_create_metas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMetasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('metas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('meta_key');
+            $table->longText('meta_value');
+            $table->morphs('model');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('metas');
+    }
+}
