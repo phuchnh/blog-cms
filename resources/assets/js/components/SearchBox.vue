5e18392cd0b5195a375853cf59fee4f5425d4803
Huỳnh Ngọc Hoàng Phúc <huynhngochoangphuc@gmail.com>
2019-04-23 19:26:10 +0700

    Handle reset form

--- resources/assets/js/components/SearchBox.vue
+++ resources/assets/js/components/SearchBox.vue
@@ -1,20 +1,26 @@
 <template>
   <div>
-    <form role="searchbox" @submit.prevent="handleSubmit">
+    <form role="search" @submit.prevent="handleSubmit">
       <div class="row">
         <div class="form-inline col-xs-12 col-md-3">
           <label for="column">Column</label>
+          {{ search.column }}
           <select class="form-control" style="width: 100%" id="column" v-model="search.column">
-            <option v-for="(column, index) in columns" :key="index" :value="column"> {{ column }}</option>
+            <option v-for="(column, index) in columns"
+                    :key="index"
+                    :value="column | lowerCase"
+                    :selected="search.column === column"> {{ column }}
+            </option>
           </select>
         </div>
         <div class="form-group col-xs-12 col-md-6">
           <label for="keyword">Keyword</label>
+          {{ search.keyword }}
           <input type="text" class="form-control" name="keyword" id="keyword" v-model="search.keyword">
         </div>
         <div class="col-xs-12 col-md-3" style="margin-top: 2.5rem">
           <button type="submit" class="btn btn-primary">Search</button>
-          <button type="reset" class="btn btn-default">Reset</button>
+          <button type="reset" @click="handleReset" class="btn btn-default">Reset</button>
         </div>
       </div>
     </form>
@@ -33,6 +39,12 @@
       },
     },
 
+    filters: {
+      lowerCase (value) {
+        return value.toLowerCase()
+      },
+    },
+
     data () {
       return {
         search: {
@@ -43,6 +55,14 @@
     },
 
     methods: {
+      handleReset () {
+        this.search.column = this.columns[0].toLocaleLowerCase()
+        this.search.keyword = null
+        let obj = {}
+        obj[this.search.column] = this.search.keyword
+        this.$emit('change', obj)
+      },
+
       handleSubmit () {
         this.search.column = this.search.column.toLocaleLowerCase()
         let obj = {}
