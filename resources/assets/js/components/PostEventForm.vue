399c4562be464db810fabfc4ba3f0257771a7f79
LeonNguyen <n.anhtuan0808@gmail.com>
2019-04-24 12:25:23 +0700

    new event box and update meta api

--- resources/assets/js/components/PostEventForm.vue
+++ resources/assets/js/components/PostEventForm.vue
@@ -45,19 +45,29 @@
         </div>
       </div>
     </div>
+    <div class="form-group" :class="{ 'has-error': errors.first('location') }">
+      <label for="location" class="col-sm-2 control-label">Location <span class="required">*</span></label>
+      <div class="col-sm-10">
+        <input class="form-control" id="location" name="location"
+               v-model="item.location" v-validate="'required'" />
+        <div class="help-block" v-if="errors.first('location')">
+          {{ errors.first('location') }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
     inject: ['$validator'],
-    name: 'PostDateForm',
+    name: 'PostEventForm',
     props: {
       value: String | Object | Array
     },
     data () {
       return {
-        item: this.value ? this.value : {},
+        item: !_.isEmpty(this.value) ? this.value : {},
       }
     },
     watch: {
