5a7ed37f43ab8d180a4acb93a37020ec034691c2
LeonNguyen <n.anhtuan0808@gmail.com>
2019-04-24 20:40:05 +0700

    remove unnecessary things on sidebar

--- resources/assets/js/layouts/Sidebar.vue
+++ resources/assets/js/layouts/Sidebar.vue
@@ -16,37 +16,13 @@
         </div>
       </div>
 
-      <!-- search form (Optional) -->
-      <form action="#" method="get" class="sidebar-form">
-        <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Search...">
-          <span class="input-group-btn">
-              <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-              </button>
-            </span>
-        </div>
-      </form>
-      <!-- /.search form -->
-
       <!-- Sidebar Menu -->
       <ul class="sidebar-menu" data-widget="tree">
-        <li class="header">HEADER</li>
         <!-- Optionally, you can add icons to the links -->
-        <router-link :to="{name: 'userList'}" tag="li" active-class="active"><a><i class="fa fa-link"></i> <span>User</span></a></router-link>
-        <router-link :to="{name: 'categoryList'}" tag="li" active-class="active"><a><i class="fa fa-link"></i> <span>Category</span></a></router-link>
-        <template v-for="item in listCategory">
-          <li class="treeview">
-            <router-link :to="{name: 'postList', query: {type: item.slug}}">
-              <i class="fa fa-link"></i> <span>{{ item.title }}</span>
-              <span class="pull-right-container">
-                <i class="fa fa-angle-left pull-right"></i>
-              </span>
-            </router-link>
-            <ul class="treeview-menu">
-              <router-link :to="{name: 'inThePressTrashList'}" tag="li" active-class="active"><a><i class="fa fa-trash"></i> <span>Trash</span></a></router-link>
-            </ul>
-          </li>
-        </template>
+        <router-link :to="{name: 'userList'}" tag="li" active-class="active"><a><i class="fa fa-link"></i>
+          <span>User</span></a></router-link>
+        <router-link :to="{name: 'categoryList'}" tag="li" active-class="active"><a><i class="fa fa-link"></i> <span>Category</span></a>
+        </router-link>
         <li class="treeview">
           <router-link :to="{name: 'inThePressList'}">
             <i class="fa fa-link"></i> <span>In The Press</span>
@@ -55,17 +31,25 @@
             </span>
           </router-link>
           <ul class="treeview-menu">
-            <router-link :to="{name: 'inThePressTrashList'}" tag="li" active-class="active"><a><i class="fa fa-trash"></i> <span>Trash</span></a></router-link>
+            <router-link :to="{name: 'inThePressTrashList'}" tag="li" active-class="active"><a><i
+                class="fa fa-trash"></i> <span>Trash</span></a></router-link>
           </ul>
         </li>
-        <router-link :to="{name: 'blogList'}" tag="li" active-class="active"><a><i class="fa fa-link"></i> <span>Blog</span></a></router-link>
-        <router-link :to="{name: 'eventList'}" tag="li" active-class="active"><a><i class="fa fa-link"></i> <span>Event</span></a></router-link>
-        <router-link :to="{name: 'guideList'}" tag="li" active-class="active"><a><i class="fa fa-link"></i> <span>Guided Meditation</span></a></router-link>
-        <router-link :to="{name: 'practiceList'}" tag="li" active-class="active"><a><i class="fa fa-link"></i> <span>Daily Practice</span></a></router-link>
-        <router-link :to="{name: 'clientList'}" tag="li" active-class="active"><a><i class="fa fa-link"></i> <span>Client</span></a></router-link>
+        <router-link :to="{name: 'blogList'}" tag="li" active-class="active"><a><i class="fa fa-link"></i>
+          <span>Blog</span></a></router-link>
+        <router-link :to="{name: 'eventList'}" tag="li" active-class="active"><a><i class="fa fa-link"></i>
+          <span>Event</span></a></router-link>
+        <router-link :to="{name: 'guideList'}" tag="li" active-class="active"><a><i class="fa fa-link"></i> <span>Guided Meditation</span></a>
+        </router-link>
+        <router-link :to="{name: 'practiceList'}" tag="li" active-class="active"><a><i class="fa fa-link"></i> <span>Daily Practice</span></a>
+        </router-link>
+        <router-link :to="{name: 'clientList'}" tag="li" active-class="active"><a><i class="fa fa-link"></i> <span>Client</span></a>
+        </router-link>
         <!--<router-link :to="{name: 'blogList'}" tag="li" active-class="active"><a><i class="fa fa-link"></i> <span>Subscriber</span></a></router-link>-->
-        <router-link :to="{name: 'faqList'}" tag="li" active-class="active"><a><i class="fa fa-link"></i> <span>FAQ</span></a></router-link>
-        <router-link :to="{name: 'setting'}" tag="li" active-class="active"><a><i class="fa fa-gear"></i> <span>Setting</span></a></router-link>
+        <router-link :to="{name: 'faqList'}" tag="li" active-class="active"><a><i class="fa fa-link"></i>
+          <span>FAQ</span></a></router-link>
+        <router-link :to="{name: 'setting'}" tag="li" active-class="active"><a><i class="fa fa-gear"></i>
+          <span>Setting</span></a></router-link>
       </ul>
       <!-- /.sidebar-menu -->
     </section>
@@ -74,28 +58,13 @@
 </template>
 
 <script>
-  import store from '@/store'
   export default {
     name: 'Sidebar',
     computed: {
       currentUser () {
         return this.$store.state.auth.currentUser
       },
-      listCategory () {
-        return this.$store.getters['taxonomy/getSidebarItem']
-      }
     },
-    mounted () {
-      store.dispatch('taxonomy/fetchSidebarList', {type: this.type})
-    },
-    data () {
-      return {
-        type: 'category'
-      }
-    }
-    // beforeRouteEnter (to, from, next) {
-    //   store.dispatch('taxonomy/fetchList').then(() => next())
-    // },
   }
 </script>
 
